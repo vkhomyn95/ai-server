97077260c2c2e174218dae41c3022b34f183e54f
vkhomyn <vkhomyn95@gmail.com>
2023-10-23 18:27:11 +0300

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rm -f ai-server
 docker build -t vk/ai-server .
-docker run -d --restart always --net=host -v /etc/localtime:/etc/localtime:ro \
+docker run -d --restart always --net=host -v /stor:/stor -v /etc/localtime:/etc/localtime:ro \
         --log-opt max-size=500m --log-opt max-file=5 \
         -e APP_HOST='127.0.0.1' \
         -e APP_PORT='50053' \
