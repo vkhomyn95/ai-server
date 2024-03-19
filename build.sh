1ac5172ae197042b6ed73a08459abfed3bb938e1
Volodymyr Khomyn <vkhomyn95@gmail.com>
2024-03-19 10:30:01 +0200

    docker host fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker rm -f ai-server
 docker build -t vk/ai-server .
 docker run -d --restart always --net=host -v /stor:/stor -v /etc/localtime:/etc/localtime:ro \
         --log-opt max-size=500m --log-opt max-file=5 \
-        -e APP_HOST='127.0.0.1' \
+        -e APP_HOST='0.0.0.0' \
         -e APP_PORT='50053' \
         -e AUDIO_DIR='/stor/data/audio/' \
         -e LOGGER_DIR='/stor/data/logs/server' \
